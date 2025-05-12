fd1603845dab9974cc99ffa879d48990dfecb963
lendrik-kumar <sanketkamboj2006@gmail.com>
2025-05-12 17:55:47 +0530

    faltu commit

---
+++ app/src/main/java/org/example/entities/Train.java
@@ -0,0 +1,20 @@
+package org.example.entities;
+
+import java.sql.Time;
+import java.util.List;
+import java.util.Map;
+import java.util.Timer;
+
+public class Train {
+
+    private String trainId;
+
+    private String trainNo;
+
+    private List<List<Integer>> seats;
+
+    private Map<String, Time> station;
+
+    private List<String> stations;
+
+}
---
+++ app/src/main/java/org/example/service/UserBookingService.java
@@ -0,0 +1,12 @@
+package org.example.service;
+
+import org.example.entities.User;
+
+public class UserBookingService {
+
+    public User user;
+
+    public UserBookingService(User user1){
+        this.user = user1;
+    }
+}
